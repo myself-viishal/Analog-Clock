e6111374730aad6d75f68c655eae02f0c57fcae7
myself-viishal <3953vishal@gmail.com>
2024-01-05 16:49:27 +0530

    added animation

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const secondHand = document.getElementById("second-hand");
 setInterval(() => {
     const time = new Date();
     let hour = 30 * (time.getHours() % 12) + 0.5 * time.getMinutes();
-    let minute = 6 * time.getMinutes();
+    let minute = 6 * time.getMinutes() + 0.1 * time.getSeconds();
     let second = 6 * time.getSeconds();
 
     secondHand.style.transform = `translate(-50%, -50%) rotate(${second}deg)`;
     minuteHand.style.transform = `translate(-50%, -50%) rotate(${minute}deg)`;
     hourHand.style.transform = `translate(-50%, -50%) rotate(${hour}deg)`;
 }, 1000);
-
